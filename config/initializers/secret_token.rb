4d50fe647be3bcc2c933cc066434bccf2bad3dfb
ChiaChia Lee <spiderman0103@gmail.com>
2012-08-26 00:00:56 +0800

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyBirthdayToBrent::Application.config.secret_token = '54910fac8a690442a20391b0a9075661c3de3982c356d5677582f9e1fa39cfbc72210ab9eec42c0e972685cddf9541e90e68b1131ae4536c8794409a609baed7'
